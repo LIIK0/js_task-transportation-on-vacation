ee423d4de51af87664ad3fa9284a3631fbb1498d
LIIK0 <li2002yaroslav@gmail.com>
2026-09-14 21:42:58 +0200

    Ok, Luke must be a Nerd

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const NUMBER_OF_DAYS = days;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
   const PRICE = days * 40;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
 
-  if (days >= 7) {
+  if (NUMBER_OF_DAYS >= LONG_TERM) {
     return PRICE - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
+  if (NUMBER_OF_DAYS >= SHORT_TERM) {
     return PRICE - SHORT_TERM_DISCOUNT;
   }
 
