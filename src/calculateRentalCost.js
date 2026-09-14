f9d411943350b9f619766793b3aaaa21d24d59c6
LIIK0 <li2002yaroslav@gmail.com>
2026-09-14 21:32:44 +0200

    eazy peazy lemon squeezy

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE = days * 40;
+
+  if (days >= 7) {
+    return PRICE - 50;
+  }
+
+  if (days >= 3) {
+    return PRICE - 20;
+  }
+
+  return PRICE;
 }
 
 module.exports = calculateRentalCost;
